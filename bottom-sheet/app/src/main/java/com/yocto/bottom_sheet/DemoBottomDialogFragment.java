a6bc82ce71c5552870ca1428f8980d2a6ac51888
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-01-18 23:19:52 +0800

    FIX

---
+++ bottom-sheet/app/src/main/java/com/yocto/bottom_sheet/DemoBottomDialogFragment.java
@@ -0,0 +1,69 @@
+package com.yocto.bottom_sheet;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class DemoBottomDialogFragment extends BottomSheetDialogFragment {
+
+    public static DemoBottomDialogFragment newInstance() {
+        return new DemoBottomDialogFragment();
+    }
+
+    @NonNull
+    @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+        // https://stackoverflow.com/questions/58651661/how-to-set-max-height-in-bottomsheetdialogfragment
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override public void onShow(DialogInterface dialogInterface) {
+                BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) dialogInterface;
+
+                FrameLayout bottomSheet = bottomSheetDialog.findViewById(com.google.android.material.R.id.design_bottom_sheet);
+
+                ViewGroup.LayoutParams layoutParams = bottomSheet.getLayoutParams();
+
+                // !!!
+                layoutParams.height = 900;
+
+                bottomSheet.setLayoutParams(layoutParams);
+            }
+        });
+
+        return  dialog;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Make the bottom sheet non dragable.
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.BottomSheetDialogStyle);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.bottom_sheet_layout, container,
+                false);
+
+        // get the views and attach the listener
+
+        return view;
+
+    }
+}
--- bottom-sheet/app/src/main/java/com/yocto/bottom_sheet/MainActivity.java
+++ bottom-sheet/app/src/main/java/com/yocto/bottom_sheet/MainActivity.java
@@ -3,6 +3,7 @@ package com.yocto.bottom_sheet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.image_button_0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                demo();
+            }
+        });
+    }
+
+    public void demo() {
+        DemoBottomDialogFragment demoBottomDialogFragment = DemoBottomDialogFragment.newInstance();
+
+        demoBottomDialogFragment.show(getSupportFragmentManager(), "demoBottomDialogFragment");
     }
 }
