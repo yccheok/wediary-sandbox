3e2ca14c1dcba24c02685c5dfa5dd8dac1873d83
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-02-02 22:16:35 +0800

    Use ViewCompat.setOnApplyWindowInsetsListener

--- keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/MainActivity.java
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/MainActivity.java
@@ -1,8 +1,17 @@
 package com.yocto.keyboard_bottom_sheet_integration;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View keyboardView = findViewById(R.id.keyboard_view);
+
+        ViewCompat.setOnApplyWindowInsetsListener(getWindow().getDecorView().getRootView(), (v, insets) -> {
+            boolean imeVisible = insets.isVisible(WindowInsetsCompat.Type.ime());
+            int imeHeight = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom;
+
+            android.util.Log.i("CHEOK", "imeVisible = " + imeVisible + ", imeHeight = " + imeHeight);
+
+            ViewGroup.LayoutParams params = keyboardView.getLayoutParams();
+            params.height = imeHeight;
+            keyboardView.setLayoutParams(params);
+
+            return insets;
+        });
     }
 }
