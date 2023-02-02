f4e8ba3bac6b05417f6a03dd75100898c0214bb2
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-02-02 16:47:44 +0800

    Initial creation.

---
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yocto.keyboard_bottom_sheet_integration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
