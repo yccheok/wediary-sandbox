bd464213e076444f44b2cba5215b38e43d244553
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-02-08 18:54:34 +0800

    Not perfect yet. Fine tuning required.

--- keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/MainActivity.java
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/MainActivity.java
@@ -1,28 +1,31 @@
 package com.yocto.keyboard_bottom_sheet_integration;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.view.Window;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsAnimationCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int keyboardHeightWhenVisible = 0;
+    private boolean keyboardVisible = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View keyboardView = findViewById(R.id.keyboard_view);
+        final View keyboardView = findViewById(R.id.keyboard_view);
 
-        View rootView = getWindow().getDecorView().getRootView();
+        final View rootView = getWindow().getDecorView().getRootView();
 
         ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
             boolean imeVisible = insets.isVisible(WindowInsetsCompat.Type.ime());
@@ -32,14 +35,50 @@ public class MainActivity extends AppCompatActivity {
                     insets.getInsets(WindowInsetsCompat.Type.ime()).bottom -
                     insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom;
 
-            android.util.Log.i("CHEOK", "imeVisible = " + imeVisible + ", imeHeight = " + imeHeight);
-
-            ViewGroup.LayoutParams params = keyboardView.getLayoutParams();
-            params.height = imeHeight;
-            keyboardView.setLayoutParams(params);
+            keyboardVisible = imeVisible;
+            if (keyboardVisible) {
+                keyboardHeightWhenVisible = imeHeight;
+            }
 
             // https://stackoverflow.com/questions/75325095/how-to-use-windowinsetscompat-correctly-to-listen-to-keyboard-height-change-in-a
             return ViewCompat.onApplyWindowInsets(v, insets);
+
         });
+
+        WindowInsetsAnimationCompat.Callback callback = new WindowInsetsAnimationCompat.Callback(
+                WindowInsetsAnimationCompat.Callback.DISPATCH_MODE_STOP
+        ) {
+            @NonNull
+            @Override
+            public WindowInsetsCompat onProgress(@NonNull WindowInsetsCompat insets, @NonNull List<WindowInsetsAnimationCompat> runningAnimations) {
+                // Find an IME animation.
+                WindowInsetsAnimationCompat imeAnimation = null;
+                for (WindowInsetsAnimationCompat animation : runningAnimations) {
+                    if ((animation.getTypeMask() & WindowInsetsCompat.Type.ime()) != 0) {
+                        imeAnimation = animation;
+                        break;
+                    }
+                }
+                if (imeAnimation != null) {
+                    int keyboardViewHeight;
+                    if (keyboardVisible) {
+                        keyboardViewHeight = (int) (keyboardHeightWhenVisible * imeAnimation.getInterpolatedFraction());
+                    } else {
+                        keyboardViewHeight = (int) (keyboardHeightWhenVisible * (1.0-imeAnimation.getInterpolatedFraction()));
+                    }
+
+                    ViewGroup.LayoutParams params = keyboardView.getLayoutParams();
+                    params.height = keyboardViewHeight;
+                    keyboardView.setLayoutParams(params);
+
+                    Log.i("CHEOK", "keyboardViewHeight = " + keyboardViewHeight);
+                }
+                return insets;
+
+            }
+        };
+
+        ViewCompat.setWindowInsetsAnimationCallback(rootView, callback);
+
     }
 }
