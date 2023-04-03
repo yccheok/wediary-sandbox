6f335858c779ba8e6409c41d1206bd7dc3ce848b
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-04-03 20:23:47 +0800

    emoji_recent

--- keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/EmojiCategory.java
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/EmojiCategory.java
@@ -1,6 +1,7 @@
 package com.yocto.keyboard_bottom_sheet_integration;
 
 public enum EmojiCategory {
+    Recent(R.drawable.emoji_recent, 0),
     People(R.drawable.emoji_ios_category_people, 1),
     Nature(R.drawable.emoji_ios_category_nature, 2),
     Food(R.drawable.emoji_ios_category_food, 3),
@@ -8,8 +9,7 @@ public enum EmojiCategory {
     Travel(R.drawable.emoji_ios_category_travel, 5),
     Objects(R.drawable.emoji_ios_category_objects, 6),
     Symbols(R.drawable.emoji_ios_category_symbols, 7),
-    Flags(R.drawable.emoji_ios_category_flags, 8),
-    Recent(R.drawable.emoji_recent, 9);
+    Flags(R.drawable.emoji_ios_category_flags, 8);
 
     EmojiCategory(int resourceId, int code) {
         this.resourceId = resourceId;
