80d73b04b7a5f7d40a9456cf5012d37450d8d133
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-04-04 15:38:51 +0800

    Initial creation.

---
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/PickerLayout.java
@@ -0,0 +1,44 @@
+package com.yocto.keyboard_bottom_sheet_integration;
+
+import android.content.Context;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class PickerLayout extends LinearLayout {
+    private final Picker picker;
+    private final PickerListener pickerListener;
+
+    private TextView titleTextView;
+    private Button closeButton;
+
+    public PickerLayout(Context context, Picker picker, PickerListener pickerListener) {
+        super(context);
+
+        this.picker = picker;
+
+        this.pickerListener = pickerListener;
+
+        init();
+    }
+
+    private void init() {
+        inflate(getContext(), R.layout.picker_layout, this);
+
+        setOrientation(VERTICAL);
+
+        this.titleTextView = findViewById(R.id.title_text_view);
+        this.closeButton = findViewById(R.id.close_button);
+
+        this.closeButton.setOnClickListener(view -> pickerListener.onPickerClosed());
+
+        if (picker == Picker.Emoji) {
+            titleTextView.setText("Emoji");
+
+            EmojiPicker emojiPicker = new EmojiPicker(this.getContext(), this.pickerListener);
+            addView(emojiPicker);
+        } else if (picker == Picker.Background) {
+            throw new RuntimeException();
+        }
+    }
+}
