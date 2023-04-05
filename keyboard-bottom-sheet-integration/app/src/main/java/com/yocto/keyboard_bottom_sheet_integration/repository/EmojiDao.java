ca3cd3087fa1f12b12ab2f9fdf3c5f41e77e0633
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-04-05 19:59:24 +0800

    getEmojis

--- keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/repository/EmojiDao.java
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/repository/EmojiDao.java
@@ -1,2 +1,15 @@
-package com.yocto.keyboard_bottom_sheet_integration.repository;public class EmojiDao {
+package com.yocto.keyboard_bottom_sheet_integration.repository;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.yocto.keyboard_bottom_sheet_integration.model.Emoji;
+
+import java.util.List;
+
+@Dao
+public interface EmojiDao {
+    @Query("SELECT * FROM emoji WHERE category = :category AND selected = 1 order by id")
+    public abstract List<Emoji> getEmojis(int category);
 }
