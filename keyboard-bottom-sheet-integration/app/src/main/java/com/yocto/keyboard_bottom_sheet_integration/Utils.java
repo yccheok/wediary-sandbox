2a8386e14364a60626110241f362ad4f3797d3b6
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-04-05 19:55:50 +0800

    Util functions.

--- keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/Utils.java
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/Utils.java
@@ -1,2 +1,17 @@
-package com.yocto.keyboard_bottom_sheet_integration;public class Utils {
+package com.yocto.keyboard_bottom_sheet_integration;
+
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+public class Utils {
+    public static int dpToPixel(float dp) {
+        DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+
+        return (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics) + 0.5);
+    }
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
 }
