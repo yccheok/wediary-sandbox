b95f371299fa03b95c05372701c8a686bfe0b1e5
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-04-18 15:45:02 +0800

    Added onGlobalLayout.

--- keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/Utils.java
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/Utils.java
@@ -3,8 +3,14 @@ package com.yocto.keyboard_bottom_sheet_integration;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 public class Utils {
+    public interface GlobalLayoutCallable {
+        void call();
+    }
+
     public static int dpToPixel(float dp) {
         DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
 
@@ -13,5 +19,22 @@ public class Utils {
 
     public static int getScreenWidth() {
         return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    public static void onGlobalLayout(View view, GlobalLayoutCallable callable) {
+        final ViewTreeObserver vto = view.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            @Override
+            public void onGlobalLayout() {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                } else {
+                    view.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+
+                callable.call();
+            }
+        });
     }
 }
