f2a4bbaa721d8f33703a20f2fa1785d42121b499
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-04-18 12:38:36 +0800

    WIP.

--- keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/EmojiPopup.java
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/EmojiPopup.java
@@ -10,26 +10,43 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.PopupWindow;
 
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.yocto.keyboard_bottom_sheet_integration.model.Emoji;
+import com.yocto.keyboard_bottom_sheet_integration.repository.EmojiRoomDatabase;
+
+import java.util.List;
 
 public class EmojiPopup {
     final View targetView;
     final Emoji emoji;
+    final EmojiPopupAdapter emojiPopupAdapter;
 
     PopupWindow popupWindow;
     View view;
+    RecyclerView recyclerView;
 
     public EmojiPopup(View targetView, Emoji emoji) {
         this.targetView = targetView;
         this.emoji = emoji;
+
+        Context context = targetView.getContext();
+        List<Emoji> emojis = EmojiRoomDatabase.instance(context).emojiDao().getEmojis(
+                emoji.group
+        );
+        this.emojiPopupAdapter = new EmojiPopupAdapter(context, emojis);
     }
 
     public void show() {
-        Log.i("CHEOK", "show!");
-
         Context context = targetView.getContext();
 
         this.view = View.inflate(context, R.layout.emoji_popup, null);
+
+        this.recyclerView = this.view.findViewById(R.id.recycler_view);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(context, 6);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(emojiPopupAdapter);
 
         popupWindow = new PopupWindow(view, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
 
@@ -44,6 +61,6 @@ public class EmojiPopup {
         int y = location[1];
         Log.i("CHEOK", x + ", " + y);
         //popupWindow.showAtLocation(targetView, Gravity.NO_GRAVITY, x, y);
-        popupWindow.showAsDropDown(targetView, 0, -280);
+        popupWindow.showAsDropDown(targetView, 0, -350);
     }
 }
