a192e7ffaf8c1ae21440fda8ae8707f113db35f2
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-04-18 16:18:30 +0800

    Use getMeasuredHeight to compute y offset.

--- keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/EmojiPopup.java
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/EmojiPopup.java
@@ -69,15 +69,17 @@ public class EmojiPopup {
             popupWindow.getContentView(). getLocationOnScreen(locations);
             int popupWindowX = locations[0];
 
+            view.measure(
+                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+            );
+            final int yOffset = -targetViewHeight-view.getMeasuredHeight();
+
             // We can now obtain the height of view. Now, we need to dismiss it first, before
             // showing the popupWindow in correct position again.
             popupWindow.dismiss();
-
-            popupWindow.setOnDismissListener(() -> Log.i("CHEOK", "Pop up window is dismissed"));
-
-            final int yOffset = -targetViewHeight-view.getHeight();
-
             popupWindow.showAsDropDown(targetView, 0, yOffset);
+            popupWindow.setOnDismissListener(() -> Log.i("CHEOK", "Pop up window is dismissed"));
 
             ImageView arrowImageView = view.findViewById(R.id.arrow_image_view);
             CardView cardView = view.findViewById(R.id.cardview);
