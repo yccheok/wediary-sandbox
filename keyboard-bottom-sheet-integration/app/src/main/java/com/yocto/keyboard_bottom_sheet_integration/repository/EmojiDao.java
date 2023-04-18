fdccc314b127219ea3049228a59e52692fd19726
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2023-04-18 11:57:14 +0800

    getEmojis from group.

--- keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/repository/EmojiDao.java
+++ keyboard-bottom-sheet-integration/app/src/main/java/com/yocto/keyboard_bottom_sheet_integration/repository/EmojiDao.java
@@ -12,4 +12,7 @@ import java.util.List;
 public interface EmojiDao {
     @Query("SELECT * FROM emoji WHERE category = :category AND selected = 1 order by id")
     public abstract List<Emoji> getEmojis(int category);
+
+    @Query("SELECT * FROM emoji WHERE `group` = :group order by id")
+    public abstract List<Emoji> getEmojis(String group);
 }
